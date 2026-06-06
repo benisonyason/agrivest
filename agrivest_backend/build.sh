3729d5e4af8aac53a3b6c164633bd34f5e27d434
Benison Yason <83278793+benisonyason@users.noreply.github.com>
2026-06-06 03:50:56 +0100

    Update build.sh

--- agrivest_backend/build.sh
+++ agrivest_backend/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Build script for Render
-
-# Exit on error
 set -o errexit
 
-# Install Python dependencies
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Run migrations
-python manage.py migrate --noinput
+echo "Build completed!"
