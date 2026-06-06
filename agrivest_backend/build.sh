2855840de57c3546ae1cdd772820fa944c6e75af
Benison Yason <83278793+benisonyason@users.noreply.github.com>
2026-06-06 03:21:20 +0100

    Add build script for Render deployment

---
+++ agrivest_backend/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Build script for Render
+
+# Exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate --noinput
